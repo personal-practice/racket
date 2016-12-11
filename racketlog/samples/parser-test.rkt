bf504c17372ed86569edcbecbeba9e27710b9781
Orestis Melkonian <melkon.or@gmail.com>
2016-12-11 01:12:38 +0100

    Racketlog: Moved to separate repository

--- racketlog/samples/parser-test.rkt
+++
@@ -1,7 +0,0 @@
-#lang br
-(require racketlog/parser
-         racketlog/tokenizer
-         brag/support)
-
-;(parse-tree (apply-tokenizer tokenize ";Silly example\n fact(1234)."))
-;(parse-tree (apply-tokenizer tokenize "//Silly example\n fact(1234).\n ?query(X)."))
